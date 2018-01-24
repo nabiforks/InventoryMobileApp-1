c1cc19ce83302f0a3927995a3814becd8d945e3d
amitmohanty56 <amitmohanty56@gmail.com>
2018-01-24 16:40:26 +0800

    Added BusinessLogic folder for business logic

--- app/src/main/java/team8/inventorymobileapp/com/inventorymobileapp/AdjustmentBySupervisor.java
+++ app/src/main/java/team8/inventorymobileapp/com/inventorymobileapp/AdjustmentBySupervisor.java
@@ -3,16 +3,13 @@ package team8.inventorymobileapp.com.inventorymobileapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import team8.inventorymobileapp.com.inventorymobileapp.BusinessLogic.StoreSupervisor;
-
-
-public class MainActivity extends AppCompatActivity {
+public class AdjustmentBySupervisor extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_adjustment_by_supervisor);
 
-        StoreSupervisor s = new StoreSupervisor();
+
     }
 }
---
+++ app/src/main/java/team8/inventorymobileapp/com/inventorymobileapp/BusinessLogic/StoreSupervisor.java
@@ -0,0 +1,8 @@
+package team8.inventorymobileapp.com.inventorymobileapp.BusinessLogic;
+
+/**
+ * Created by Amit on 24/1/2018.
+ */
+
+public class StoreSupervisor {
+}
